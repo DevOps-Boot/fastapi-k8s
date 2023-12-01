850c18dcb9b69d33a4107dfd84322ebab1906256
gitkvark <github@kvark.fr>
2023-12-01 10:11:58 +0000

    Clean up helm packages

--- terraform/deployments/preprod/releases/fastapi_app.tf
+++ terraform/deployments/preprod/releases/fastapi_app.tf
@@ -1,9 +1,9 @@
 resource "helm_release" "fastapi_app" {
   name             = "fastapi-app"
-  chart            = "${path.module}/../../../../helm"
+  chart            = "${path.module}/../../../../helm/fastapi-app"
   create_namespace = true
   values = [
-    file("${path.module}/../../../../helm/values-preprod.yaml")
+    file("${path.module}/../../../../helm/fastapi-app/values-preprod.yaml")
   ]
   description = var.cert_manager_id # Used for dependency on cert_manager
   depends_on  = [helm_release.postgres_cluster]
