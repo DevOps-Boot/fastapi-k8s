bbf9182696c486ccda72374a2df5b4981247a9e8
gitkvark <github@kvark.fr>
2023-12-15 13:59:04 +0100

    Setting secrets as sensitive

--- terraform/deployments/releases/variables.tf
+++ terraform/deployments/releases/variables.tf
@@ -2,9 +2,11 @@
 variable "s3_backup_aws_access_key_id" {
   description = "AWS access key ID for the S3 bucket"
   type        = string
+  sensitive   = true
 }
 
 variable "s3_backup_aws_secret_access_key" {
   description = "AWS secret access key for the S3 bucket"
   type        = string
+  sensitive   = true
 }
