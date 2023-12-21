ca7f0f4585b46c93dabde8e42b13f599c8072b75
gitkvark <146422227+gitkvark@users.noreply.github.com>
2023-12-21 15:48:29 +0100

    Fix postgres-cluster path
    Fixing path to actually point to chart.

--- terraform/deployments/preprod/releases/postgres_cluster.tf
+++ terraform/deployments/preprod/releases/postgres_cluster.tf
@@ -1,6 +1,6 @@
 resource "helm_release" "postgres_cluster" {
   name             = "postgres-cluster"
-  chart            = "${path.module}/../../../../helm/postgres"
+  chart            = "${path.module}/../../../../helm/postgres-cluster"
   create_namespace = true
   values = [
     file("${path.module}/../../../../helm/postgres-cluster/values-preprod.yaml")
