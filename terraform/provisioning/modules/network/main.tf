e019a10f218a206224f3e9b9a767db1e3a1c6e3e
gitkvark <github@kvark.fr>
2023-12-29 15:40:31 +0100

    Enabling one NAT GW per AZ

--- terraform/provisioning/modules/network/main.tf
+++ terraform/provisioning/modules/network/main.tf
@@ -10,9 +10,10 @@ module "vpc" {
   private_subnets = var.private_subnets
   public_subnets  = var.public_subnets
 
-  enable_nat_gateway   = true
-  single_nat_gateway   = true
-  enable_dns_hostnames = true
+  enable_nat_gateway     = true
+  single_nat_gateway     = false
+  one_nat_gateway_per_az = true
+  enable_dns_hostnames   = true
 
   public_subnet_tags = {
     "kubernetes.io/cluster/${var.cluster_name}" = "shared"
